156e4f0a15940ba8581846c155138fe477ffbbb9
1234567890asdf <49981626+1234567890asdf@users.noreply.github.com>
2019-08-14 11:55:17 -0300

    Add files via upload

--- MinhaClasse.java
+++ MinhaClasse.java
@@ -9,15 +9,15 @@ public class MinhaClasse {
     public static void main(String[] args) {
         Estudante meuNome = new Estudante();
         Scanner input = new Scanner( System.in );
-        meuNome.nome = "Cristiano Gomes";
-        meuNome.endereço = "Rua gaivota quadra 11 casa 13 Parque Lago";
-        meuNome.idade = 20;
-        meuNome.notaMatematica = 6;
-        meuNome.notaPortugues = 8;
-        meuNome.notaCiencias = 7;
-        System.out.printf("nome: %s\n", meuNome.nome);
-        System.out.printf("endereço: %s\n", meuNome.endereço);
-        System.out.printf("idade: %s\n", meuNome.idade);
+        meuNome.setNome("Cristiano Gomes");
+        meuNome.setEndereço("Rua gaivota quadra 11 casa 13 Parque Lago");
+        meuNome.setIdade(20);
+        meuNome.setNotaMatematica(6);
+        meuNome.setNotaPortugues(8);
+        meuNome.setNotaCiencias(7);
+        System.out.printf("nome: %s\n", meuNome.getNome());
+        System.out.printf("endereço: %s\n", meuNome.getEndereço());
+        System.out.printf("idade: %s\n", meuNome.getIdade());
         System.out.printf("notaMatematica: %s\n", meuNome.notaMatematica);
         System.out.printf("notaPortugues: %s\n", meuNome.notaPortugues);
         System.out.printf("notaCiencias: %s\n", meuNome.notaCiencias);
@@ -31,3 +31,7 @@ public class MinhaClasse {
     }
     
 }
+/*public double getMedia (){
+        double result = 0;
+        result = (notaMatematica+notaPortugues+notaCiencias)/3;
+        return result;*/
